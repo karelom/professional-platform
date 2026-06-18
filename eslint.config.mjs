0a8da35181d1bf851f042fa9ab3db2626bf46c41
David <david851215@gmail.com>
2026-06-18 21:30:31 +0800

    init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default withNuxt(eslintPluginPrettier)
